2246d6ba49a25a6d9489c845907601c559a7d37c
Wendel <wendelljuliao@hotmail.com>
2023-06-17 12:08:59 -0300

    colocando de volta o algoritmo flooding search sem recursividade comentado

--- src/search-algorithms/flood-search.ts
+++ src/search-algorithms/flood-search.ts
@@ -57,6 +57,48 @@ export function floodSearch(graph: Graph, startNode: string, targetResource: str
     return search();
 }
 
+/* export function floodSearch(graph: Graph, startNode: string, targetResource: string, TTL: number): { node: string | null, visited: number, path: Array<any> } {
+    if(TTL === 0) {
+        return { node: null, visited: 0, path: [] };
+    }
+
+    TTL += 1;
+
+    let queue: SearchNode[] = [{ node: startNode, depth: 0 }];
+    let visited = new Set<string>();
+
+    while(queue.length > 0) {
+        if(TTL === 0) {
+            return { node: null, visited: visited.size - 1, path: Array.from(visited.values()) };
+        }
+        let current = queue.shift();
+        if(current) {
+            let { node, depth } = current;
+
+            if(visited.has(node))
+                continue;
+
+            visited.add(node);
+            console.log("Visitando nó", node)
+            let resources = graph.getResources(node);
+
+            if(resources.includes(targetResource)) {
+                return { node, visited: visited.size - 1, path: Array.from(visited.values()) };
+            }
+
+            if(depth < TTL) {
+                let neighbors = graph.getNeighbors(node);
+                for(let neighbor of neighbors) {
+                    queue.push({ node: neighbor, depth: depth + 1 });
+                }
+            }
+        TTL -= 1;
+        }
+    }
+
+    return { node: null, visited: visited.size, path: Array.from(visited.values()) };
+} */
+
 export function cacheFloodSearch(graph: Graph, startNode: string, targetResource: string, ttl: number): { node: string | null, visited: number, path: Array<any> } {
     const queue: Array<{ node: string, ttl: number }> = [{ node: startNode, ttl: ttl }];
     const visited = new Set<string>();
